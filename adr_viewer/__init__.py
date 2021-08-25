7cd814a102f21549f782d7547de4f65257807034
Anis Campos <anis@lumapps.com>
2021-08-25 16:54:57 +0200

    chore(typo): Superseded
    fix tests remove and all superceded occurrences

--- adr_viewer/__init__.py
+++ adr_viewer/__init__.py
@@ -35,8 +35,8 @@ def parse_adr_to_config(path):
         status = 'amended'
     elif any([line.startswith("Accepted") for line in status]):
         status = 'accepted'
-    elif any([line.startswith("Superceded by") for line in status]):
-        status = 'superceded'
+    elif any([line.startswith("Superseded by") for line in status]):
+        status = 'superseded'
     elif any([line.startswith("Pending") for line in status]):
         status = 'pending'
     else:

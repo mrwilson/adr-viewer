08c406d8eb0eea3d59b001c942e62000b82034a1
alshapton <alshapton@gmail.com>
2022-08-11 13:13:38 +0100

    (feature): Introduced ability to use a configuration file for the colours of bars etc)

--- adr_viewer/__init__.py
+++ adr_viewer/__init__.py
@@ -1,11 +1,14 @@
+import os
 import glob
+import toml
+import ast
+
+from bottle import Bottle, run
+from bs4 import BeautifulSoup
+import click
+from jinja2 import Environment, PackageLoader, select_autoescape
 from jinja2.loaders import FileSystemLoader
 import mistune
-import os
-from bs4 import BeautifulSoup
-from jinja2 import Environment, PackageLoader, select_autoescape
-import click
-from bottle import Bottle, run
 
 
 def extract_statuses_from_adr(page_object):
@@ -58,7 +61,7 @@ def parse_adr_to_config(path):
 def render_html(config, template_dir_override=None):
 
     env = Environment(
-        loader=PackageLoader('adr_viewer', 'templates') if template_dir_override is None else FileSystemLoader(template_dir_override),
+        loader = PackageLoader('adr_viewer', 'templates') if template_dir_override is None else FileSystemLoader(template_dir_override),
         autoescape=select_autoescape(['html', 'xml'])
     )
 
@@ -80,7 +83,8 @@ def run_server(content, port):
     run(app, host='localhost', port=port, quiet=True)
 
 
-def generate_content(path, template_dir_override=None, heading=None):
+def generate_content(path, template_dir_override=None,
+                     heading=None, configuration=None):
 
     files = get_adr_files("%s/*.md" % path)
 
@@ -91,6 +95,30 @@ def generate_content(path, template_dir_override=None, heading=None):
         'heading': heading,
         'records': []
     }
+
+    # Set defaults for colours (or use passed in configuration)
+    conf = {}
+    if type(configuration) == type(None):
+        conf = ast.literal_eval('{ \
+        "accepted": {"background-color": "lightgreen"}, \
+        "amended": {"background-color": "yellow"}, \
+        "pending": {"background-color": "lightblue"}, \
+        "superseded": { \
+            "background-color": "lightgrey", \
+            "text-decoration": "line-through"}, \
+        "unknown": {"background-color": "white"}}')
+    else:
+        conf = configuration['status']
+
+    print(type(conf))
+    print(conf)
+    # Retrieve properties from configuration
+    for status in conf:
+        properties = {}
+        for property in conf[status]:
+            properties[property] = conf[status][property]
+        config[status] = properties
+
     for index, adr_file in enumerate(files):
 
         adr_attributes = parse_adr_to_config(adr_file)
@@ -106,20 +134,37 @@ def generate_content(path, template_dir_override=None, heading=None):
 
 
 @click.command()
-@click.option('--adr-path',      default='doc/adr/',      help='Directory containing ADR files.',         show_default=True)
-@click.option('--output',        default='index.html',    help='File to write output to.',                show_default=True)
-@click.option('--serve',         default=False,           help='Serve content at http://localhost:8000/', is_flag=True)
-@click.option('--port',          default=8000,            help='Change port for the server',              show_default=True)
-@click.option('--template-dir',  default=None,            help='Template directory.',                     show_default=True)
-@click.option('--heading',       default='ADR Viewer - ', help='ADR Page Heading',                        show_default=True)
-def main(adr_path, output, serve, port, template_dir, heading):
-    content = generate_content(adr_path, template_dir, heading)
+@click.option('--adr-path',      default='doc/adr/',
+              help='Directory containing ADR files.', show_default=True)
+@click.option('--output',        default='index.html',
+              help='File to write output to.', show_default=True)
+@click.option('--serve',         default=False,
+              help='Serve content at http://localhost:8000/', is_flag=True)
+@click.option('--port',          default=8000,
+              help='Change port for the server', show_default=True)
+@click.option('--template-dir',  default=None,
+              help='Template directory.', show_default=True)
+@click.option('--heading',       default='ADR Viewer - ',
+              help='ADR Page Heading', show_default=True)
+@click.option('--config',        default='config.toml',
+              help='Configuration settings', show_default=True)
+def main(adr_path, output, serve, port, template_dir, heading, config):
+    from os.path import exists
+    # Ensure that there is a configuration file
+    if exists(config):
+        configuration_file = toml.load(config)
+    else:
+        configuration_file = None
+
+    content = generate_content(adr_path, template_dir,
+                               heading, configuration_file)
 
     if serve:
         run_server(content, port)
     else:
         with open(output, 'w') as out:
             out.write(content)
+
 
 if __name__ == '__main__':
     main()

55f8ecf8dadca0360b388a492a43b188837265cd
Alex Wilson <gpg@probablyfine.co.uk>
2023-09-23 11:50:17 +0100

    ci(format): Apply `black` to codebase
    Formatting is turned off for the cli options block in `__init__.py` because I want them manually formatted and aligned for readability

--- adr_viewer/parse.py
+++ adr_viewer/parse.py
@@ -14,17 +14,17 @@ class Adr:
 
 
 def extract_statuses_from_adr(page_object) -> Iterator[str]:
-    status_section = page_object.find('h2', text='Status')
+    status_section = page_object.find("h2", text="Status")
 
     if status_section and status_section.nextSibling:
         current_node = status_section.nextSibling
 
-        while current_node.name != 'h2' and current_node.nextSibling:
+        while current_node.name != "h2" and current_node.nextSibling:
             current_node = current_node.nextSibling
 
-            if current_node.name == 'p':
+            if current_node.name == "p":
                 yield current_node.text
-            elif current_node.name == 'ul':
+            elif current_node.name == "ul":
                 yield from (li.text for li in current_node.children if li.name == "li")
             else:
                 continue
@@ -33,22 +33,24 @@ def extract_statuses_from_adr(page_object) -> Iterator[str]:
 def parse_adr(content: str) -> Optional[Adr]:
     adr_as_html = mistune.markdown(content)
 
-    soup = BeautifulSoup(adr_as_html, features='html.parser')
+    soup = BeautifulSoup(adr_as_html, features="html.parser")
 
     statuses = list(extract_statuses_from_adr(soup))
 
     if any([line.startswith("Amended by") for line in statuses]):
-        status = 'amended'
+        status = "amended"
     elif any([line.startswith("Accepted") for line in statuses]):
-        status = 'accepted'
+        status = "accepted"
     elif any([line.startswith("Superseded by") for line in statuses]):
-        status = 'superseded'
-    elif any([line.startswith("Proposed") or line.startswith("Pending") for line in statuses]):
-        status = 'pending'
+        status = "superseded"
+    elif any(
+        [line.startswith("Proposed") or line.startswith("Pending") for line in statuses]
+    ):
+        status = "pending"
     else:
-        status = 'unknown'
+        status = "unknown"
 
-    header = soup.find('h1')
+    header = soup.find("h1")
 
     if header:
         return Adr(header.text, status, adr_as_html)
--- adr_viewer/server.py
+++ adr_viewer/server.py
@@ -2,7 +2,7 @@ from bottle import Bottle, run
 
 
 def run_server(content, port) -> None:
-    print(f'Starting server at http://localhost:{port}/')
+    print(f"Starting server at http://localhost:{port}/")
     app = Bottle()
-    app.route('/', 'GET', lambda: content)
-    run(app, host='localhost', port=port, quiet=True)
+    app.route("/", "GET", lambda: content)
+    run(app, host="localhost", port=port, quiet=True)

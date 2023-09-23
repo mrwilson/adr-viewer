a2d9c1b2da1a3fb4bc940e0f9cb16bce64c64ef0
Alex Wilson <gpg@probablyfine.co.uk>
2023-09-23 12:17:35 +0100

    chore(support): Add 3.{10,11} to `setup.py`
    This is now in line with the pre-merge checks

--- setup.py
+++ setup.py
@@ -32,6 +32,8 @@ setup(
         'Topic :: Software Development :: Documentation',
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
     ],
     use_scm_version=True,
     setup_requires=["setuptools_scm"],

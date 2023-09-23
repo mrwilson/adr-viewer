c2061fe38c65dda87a930947f876a5e8e2d4ea2c
Alex Wilson <gpg@probablyfine.co.uk>
2023-09-23 11:50:17 +0100

    ci(format): Apply `black` to codebase
    Formatting is turned off for the cli options block in `__init__.py` because I want them manually formatted and aligned for readability

--- adr_viewer/test_render.py
+++ adr_viewer/test_render.py
@@ -3,38 +3,30 @@ from adr_viewer.parse import Adr
 
 
 def test_should_render_html_with_project_title():
-    html = render_html({
-        'project_title': 'my-project'
-    })
+    html = render_html({"project_title": "my-project"})
 
-    assert '<title>ADR Viewer - my-project</title>' in html
+    assert "<title>ADR Viewer - my-project</title>" in html
 
 
 def test_should_render_html_with_record_status():
-    adr = Adr('title', 'accepted', 'content')
-    html = render_html({
-        'records': [adr]
-    })
+    adr = Adr("title", "accepted", "content")
+    html = render_html({"records": [adr]})
 
     assert '<div class="panel-heading adr-accepted">' in html
 
 
 def test_should_render_html_with_record_body():
-    adr = Adr('title', 'status', '<h1>This is my ADR</h1>')
+    adr = Adr("title", "status", "<h1>This is my ADR</h1>")
 
-    html = render_html({
-        'records': [adr]
-    })
+    html = render_html({"records": [adr]})
 
     assert '<div class="panel-body"><h1>This is my ADR</h1></div>' in html
 
 
 def test_should_render_html_with_collapsible_index():
-    adr = Adr('Record 123', 'status', 'content')
+    adr = Adr("Record 123", "status", "content")
     adr.index = 123
 
-    html = render_html({
-        'records': [adr]
-    })
+    html = render_html({"records": [adr]})
 
     assert '<a data-toggle="collapse" href="#collapse123">Record 123</a>' in html

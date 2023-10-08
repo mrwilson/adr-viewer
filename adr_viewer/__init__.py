6f82b1758fb298adee0d25978d2695cee147ebd1
Alex Wilson <gpg@probablyfine.co.uk>
2023-10-08 14:00:13 +0100

    refactor(init): Use |= instead of if-statement for merging include_mermaid

--- adr_viewer/__init__.py
+++ adr_viewer/__init__.py
@@ -28,8 +28,7 @@ def generate_content(path, template_dir_override=None, title=None) -> str:
 
         if adr_attributes:
             adr_attributes.index = index
-            if not config.include_mermaid:
-                config.include_mermaid = adr_attributes.includes_mermaid
+            adr_attributes.includes_mermaid |= config.include_mermaid
 
             config.records.append(adr_attributes)
         else:
